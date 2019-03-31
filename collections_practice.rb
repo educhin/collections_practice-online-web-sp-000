09d472c6bafcc6f5a97060e9f44823e31afcd1c9
Chinedu Arinze <c.s.arinze@gmail.com>
2019-03-31 22:07:17 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,24 @@
+def sort_array_asc(ints)
+  ints.sort
+end
 
+def sort_array_desc(ints)
+  ints.sort do |a, b|
+    b <=> a
+  end
+end
+
+def sort_array_char_count(words)
+  words.sort do |a, b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(ints)
+  ints.reverse!
+end
